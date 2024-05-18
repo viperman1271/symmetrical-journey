84c6ecd96cf855eda429e45ea6d304594eb19218
Michael Filion <viperman1271@hotmail.com>
2024-05-18 13:29:12 -0400

    Adding explicit work

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -124,31 +124,65 @@ namespace SymmetricalJourney
                 }
                 else
                 {
-                    using (WaveFileReader reader = new WaveFileReader(File))
-                    {
-                        int bytesPerSample = reader.WaveFormat.BitsPerSample / 8;
-
-                        WaveFormat newFormat = new WaveFormat(reader.WaveFormat.SampleRate, reader.WaveFormat.BitsPerSample, NUMBER_OF_CHANNELS);
-                        using(WaveFileWriter writer = new WaveFileWriter(Path.ChangeExtension(File, ".modifie.wav"), newFormat))
-                        {
-                            int currentOffset = 0;
-                            while(reader.Position < reader.Length)
-                            {
-                                byte[] data = new byte[bytesPerSample * reader.WaveFormat.Channels];
-                                reader.Read(data, 0, data.Length);
-
-                                currentOffset += data.Length;
-
-                                ReadOnlySpan<byte> convertedValues = ConvertValues(data, bytesPerSample, reader.WaveFormat.Channels);
-                                writer.Write(convertedValues);
-                            }
-                        }
-                    }
+                    ConvertFile(File, SelectTemplate);
                 }
             });
         }
 
-        private void CopyToChannel(byte[] dst, int dstChannel, byte[] src, int srcChannel, int bytesPerSample)
+        private static void ConvertFile(string inputFile, string template)
+        {
+            using (WaveFileReader reader = new WaveFileReader(inputFile))
+            {
+                int bytesPerSample = reader.WaveFormat.BitsPerSample / 8;
+
+                WaveFormat newFormat = new WaveFormat(reader.WaveFormat.SampleRate, reader.WaveFormat.BitsPerSample, NUMBER_OF_CHANNELS);
+                using (WaveFileWriter writer = new WaveFileWriter(Path.ChangeExtension(inputFile, ".modifie.wav"), newFormat))
+                {
+                    int currentOffset = 0;
+                    while (reader.Position < reader.Length)
+                    {
+                        byte[] data = new byte[bytesPerSample * reader.WaveFormat.Channels];
+                        reader.Read(data, 0, data.Length);
+
+                        currentOffset += data.Length;
+
+                        ReadOnlySpan<byte> convertedValues = ConvertValues(template, data, bytesPerSample, reader.WaveFormat.Channels);
+                        writer.Write(convertedValues);
+                    }
+                }
+            }
+        }
+
+        public ICommand ExecuteAll
+        {
+            get => new DelegateCommand(delegate
+            {
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Campagne automne.wav", "Automne");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Fleuve automne.wav", "Automne");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Nature automne.wav", "Automne");
+
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Nature été.wav", "Été");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Orage 5.1.wav", "Été");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Pluie 5.1.wav", "Été");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Vent feuillu 01.wav", "Été");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Vent feuillu 02.wav", "Été");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Grillons été nature.wav", "Été");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Fleuve été.wav", "Été");
+
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Flocons de neige.wav", "Hiver");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Glace bord de fleuve.wav", "Hiver");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Vent neige.wav", "Hiver");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Vent toundra.wav", "Hiver");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\Vent conifère - Saint-Côme.wav", "Hiver");
+
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Nature printemps.wav", "Printemps");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Milieu humide carouges QUAD.wav", "Printemps");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Campagne printemps.wav", "Printemps");
+                ConvertFile(@"C:\git\Seasons_WwiseProject\Originals\SFX\MCQ_VIVRES_Fleuve printemps.wav", "Printemps");
+            });
+        }
+
+        private static void CopyToChannel(byte[] dst, int dstChannel, byte[] src, int srcChannel, int bytesPerSample)
         {
             for(int i = 0; i < bytesPerSample; ++i)
             {
@@ -156,49 +190,49 @@ namespace SymmetricalJourney
             }
         }
 
-        private void ConvertValuesForSaison(byte[] srcValues, byte[] dstValues, int srcChannelCount, int[] dstChannelConfig, int bytesPerSample)
+        private static void ConvertValuesForSaison(byte[] srcValues, byte[] dstValues, int srcChannelCount, int[] dstChannelConfig, int bytesPerSample)
         {
             if (srcChannelCount == 2)
             {
-                CopyToChannel(dstValues, 0, srcValues, 0, bytesPerSample);
-                CopyToChannel(dstValues, 3, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 18, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 19, srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[0], srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[1], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[2], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[3], srcValues, 0, bytesPerSample);
             }
             else if (srcChannelCount == 3)
             {
-                CopyToChannel(dstValues, 0, srcValues, 0, bytesPerSample);
-                CopyToChannel(dstValues, 3, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 18, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 19, srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[0], srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[1], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[2], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[3], srcValues, 0, bytesPerSample);
             }
             else if (srcChannelCount == 4)
             {
-                CopyToChannel(dstValues, 0, srcValues, 0, bytesPerSample);
-                CopyToChannel(dstValues, 3, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 18, srcValues, 2, bytesPerSample);
-                CopyToChannel(dstValues, 19, srcValues, 3, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[0], srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[1], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[2], srcValues, 2, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[3], srcValues, 3, bytesPerSample);
             }
             else if (srcChannelCount == 5)
             {
-                CopyToChannel(dstValues, 0, srcValues, 0, bytesPerSample);
-                CopyToChannel(dstValues, 3, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 18, srcValues, 3, bytesPerSample);
-                CopyToChannel(dstValues, 19, srcValues, 4, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[0], srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[1], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[2], srcValues, 3, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[3], srcValues, 4, bytesPerSample);
             }
             else if (srcChannelCount == 6)
             {
-                CopyToChannel(dstValues, 0, srcValues, 0, bytesPerSample);
-                CopyToChannel(dstValues, 3, srcValues, 1, bytesPerSample);
-                CopyToChannel(dstValues, 18, srcValues, 4, bytesPerSample);
-                CopyToChannel(dstValues, 19, srcValues, 5, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[0], srcValues, 0, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[1], srcValues, 1, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[2], srcValues, 4, bytesPerSample);
+                CopyToChannel(dstValues, dstChannelConfig[3], srcValues, 5, bytesPerSample);
             }
         }
 
-        private byte[] ConvertValues(byte[] srcValues, int bytesPerSample, int srcChannelCount)
+        private static byte[] ConvertValues(string template, byte[] srcValues, int bytesPerSample, int srcChannelCount)
         {
             byte[] dstValues = new byte[bytesPerSample * NUMBER_OF_CHANNELS];
-            switch (SelectTemplate)
+            switch (template)
             {
                 case "Été":
                     ConvertValuesForSaison(srcValues, dstValues, srcChannelCount, [0, 3, 18, 19], bytesPerSample);
